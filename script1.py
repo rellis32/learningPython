e098a3b590ed5ac0c9c79231f684f8112a6afa74
rellis32 <bob_ellis@me.com>
2013-10-28 06:44:51 -0400

    Commit 1

---
+++ script1.py
@@ -0,0 +1,6 @@
+# A first Python script
+import sys # Load a library module
+print(sys.platform) # Get the system platform
+print(2 ** 100) # Raise 2 to a power
+x = 'Spam!'
+print(x * 8) # String repetition
